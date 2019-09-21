bb432c945777a6cff200db9cd818ab0131a4f4ba
JasonTByrd <49703410+JasonTByrd@users.noreply.github.com>
2019-09-21 14:44:00 -0500

    picker class updates
    Should now work with all three js environments.

--- src/test-component/test-component.js
+++ src/test-component/test-component.js
@@ -3,6 +3,67 @@ import './test-component.css';
 import * as THREE from "three";
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 
+
+
+
+
+class PickHelper {
+
+  constructor(scene, camera) {
+    this.pickPosition = {x: 0, y: 0};
+    this.raycaster = new THREE.Raycaster();
+    this.pickedObject = null;
+    this.pickedObjectSavedColor = 0;
+    this.scene = scene;
+    this.camera = camera;
+  }
+
+  pick(event, scene, camera, mount) {
+    this.setPickPosition(event, mount);
+    let normalizedPosition = this.pickPosition;
+    // cast a ray through the frustum
+    this.raycaster.setFromCamera(normalizedPosition, camera);
+    // get the list of objects the ray intersected
+    const intersectedObjects = this.raycaster.intersectObjects(scene.children);
+    if (intersectedObjects.length) {
+      this.pickedObject = intersectedObjects[0].object;
+    }
+    else {
+      this.pickedObject = false;
+    }
+    return this.pickedObject;
+  }
+
+  setPickPosition = (event, mount) => {
+    const pos = this.getCanvasRelativePosition(event);
+    this.pickPosition.x = (pos.x / mount.clientWidth ) *  2 - 1;
+    this.pickPosition.y = (pos.y / mount.clientHeight) * -2 + 1;  // note we flip Y
+  }
+
+  getCanvasRelativePosition = (event) => {
+    const rect = document.querySelector('.pickableCanvas').getBoundingClientRect();
+    return {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    };
+  }
+
+  clearPickPosition = () => {
+    // unlike the mouse which always has a position
+    // if the user stops touching the screen we want
+    // to stop picking. For now we just pick a value
+    // unlikely to pick something
+    this.pickPosition.x = -100000;
+    this.pickPosition.y = -100000;
+  }
+
+}
+
+
+
+
+
+
 class TestComponent extends Component {
 
   scene; 
@@ -12,8 +73,9 @@ class TestComponent extends Component {
   meshFloor;
   controls;
   mount;
+  pickHelper;
   
-  keyboard = new Array(100).fill(false);;
+  keyboard = new Array(100).fill(false);
   player;
 
   componentDidMount() {
@@ -23,6 +85,7 @@ class TestComponent extends Component {
     window.addEventListener('keydown', this.keyDown);
     window.addEventListener('keyup', this.keyUp);
     window.addEventListener('keyup', this.keyUp);
+    window.addEventListener('resize', this.onWindowResize, false );
 
     this.useWireframe = false;
     this.player = { height:1.8, speed:0.1, turnSpeed:Math.PI*0.02 }
@@ -47,10 +110,17 @@ class TestComponent extends Component {
     this.camera.lookAt(new THREE.Vector3(0,this.player.height,0));
     
     this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(1280, 720);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.mount.appendChild(this.renderer.domElement);
 
     this.controls = new PointerLockControls(this.camera);
+
+    this.pickHelper = new PickHelper(this.scene, this.camera);
+    console.log(this.scene, this.pickHelper.scene)
+
+    window.addEventListener('click', this.clickOnObject);
+
+    this.onWindowResize();
     
     this.animate();
   }
@@ -81,11 +151,23 @@ class TestComponent extends Component {
   }
 
   canvasClick = () => {
-    if(!this.controls.lock(false)) {
-      this.controls.lock(false);
-    }
+    //this.controls.lock(false);
   }
 
+  clickOnObject = (event) => {
+    //console.log('test', event);
+    let picked = this.pickHelper.pick(event, this.scene, this.camera, this.mount);
+    if (picked) {
+      if (picked.material.wireframe === true) {
+        picked.material.wireframe = false;
+      }
+      else {
+        picked.material.wireframe = true;
+      }
+    }
+    console.log(this.pickHelper.pick(event, this.scene, this.camera, this.mount));
+  }
+  
   keyDown = (event) => {
     this.keyboard[event.keyCode] = true;
   }
@@ -103,7 +185,7 @@ class TestComponent extends Component {
   render() {
     return (
       <div className="page-container">
-        <div ref={ref => (this.mount = ref)} onClick={e => this.canvasClick(e)}></div>
+        <div className="pickableCanvas" ref={ref => (this.mount = ref)} onClick={e => this.canvasClick(e)}></div>
 
         <div className="textbox-popup">
           <p>
